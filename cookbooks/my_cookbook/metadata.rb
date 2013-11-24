d883e92582d9585f18dbc558432cf192dbbb457e
Bert Pattyn <bert@dextrose.be>
2013-11-24 22:09:40 +0100

    add cookbook 'my_cookbook'

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'my_cookbook'
+maintainer       'Dextrose'
+maintainer_email 'bert@dextrose.be'
+license          'Apache 2.0'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
